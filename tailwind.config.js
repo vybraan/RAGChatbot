1cbcace675d57467b2a08beb1d50a4c0bd6465f6
Ailton Baúque <ailtonbauque@outlook.com>
2024-10-14 04:18:16 +0200

    tailwindsupport

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "index.html",
+    "main.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+}
+
